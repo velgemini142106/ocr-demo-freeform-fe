554722de03b0760577d51c31bf44242c3f4df3ed
therealtuyen <tuyendev@gmail.com>
2023-08-31 10:35:53 +0700

    add source

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DefaultLayout from "@/components/layout/DefaultLayout.vue"
+</script>
+
+<template>
+    <default-layout>
+      <template #content>
+        Hello from other side
+      </template>
+    </default-layout>
+</template>
+
+<style scoped></style>
